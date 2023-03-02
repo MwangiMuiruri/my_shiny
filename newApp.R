3b68ccf1c55773031a76dbf482ab0130d4b7df3a
MwangiMuiruri <charlesmuiruri48@gmail.com>
2023-03-02 14:37:46 +0300

    Initial commit

---
+++ newApp.R
@@ -0,0 +1,31 @@
+pacman::p_load(shiny,tidyverse,readxl, here, install = TRUE, update = getOption("pac_update"))
+shinydat<-read_excel(here("participants.xlsx"))|>
+  select(1:8)
+ui<-fluidPage(
+  titlePanel("Participants Dashboard!"),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+               information from the 2010 US Census."),
+      selectInput("var", h3("Choose a variable to display"), 
+                  choices = list("Percent white" , "Percent Black" ,
+                                 "Percent Hispanic","Percent Asian" ), selected = 1),
+      sliderInput("sliid",label="Range of interest",min = 0,max = 100,value = c(0,100))
+    
+  ),
+  mainPanel(
+    textOutput("selected_var"),
+    textOutput("selected_sliid")
+  ))
+)
+
+server<-function(input, output,session){
+  output$selected_var <- renderText({ 
+    paste("You have selected", input$var)
+  })
+  output$selected_sliid <- renderText({ 
+    paste("You have chosen a range that goes from", input$sliid[1],"to",input$sliid[2])
+  })
+}
+
+shinyApp(ui = ui, server = server)
